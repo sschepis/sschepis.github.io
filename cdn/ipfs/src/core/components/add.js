84ddd36a1b7cb3372f37159e01c8a6f498c6bb12
Sebastian Schepis <sschepis@gmail.com>
2020-08-25 09:10:15 -0700

    updates - now with runkit

--- cdn/ipfs/src/core/components/add.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-const last = require('it-last')
-
-module.exports = ({ addAll }) => {
-  return async function add (source, options) { // eslint-disable-line require-await
-    return last(addAll(source, options))
-  }
-}
--- cdn/webcrypto-core/build/types/rsa/base.d.ts
+++
@@ -1,9 +0,0 @@
-import { ProviderCrypto } from "../provider";
-export declare abstract class RsaProvider extends ProviderCrypto {
-    hashAlgorithms: string[];
-    checkGenerateKeyParams(algorithm: RsaHashedKeyGenParams): void;
-    checkImportParams(algorithm: RsaHashedImportParams): void;
-    abstract onGenerateKey(algorithm: RsaHashedKeyGenParams, extractable: boolean, keyUsages: KeyUsage[]): Promise<CryptoKeyPair>;
-    abstract onExportKey(format: KeyFormat, key: CryptoKey): Promise<JsonWebKey | ArrayBuffer>;
-    abstract onImportKey(format: KeyFormat, keyData: JsonWebKey | ArrayBuffer, algorithm: RsaHashedImportParams, extractable: boolean, keyUsages: KeyUsage[]): Promise<CryptoKey>;
-}
